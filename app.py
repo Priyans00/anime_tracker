6943203a53b858e95dd5c137d0336ba0160ae5ee
Priyans00 <115811175+Priyans00@users.noreply.github.com>
2025-01-10 20:33:15 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+def fetch_anime_details(anime_name):
+    url = "https://graphql.anilist.co"
+    query = """
+    query ($search: String) {
+        Media(search: $search, type: ANIME) {
+            id
+            title {
+                romaji
+                english
+                native
+            }
+            description
+            episodes
+            genres
+            averageScore
+            coverImage {
+                large
+            }
+        }
+    }
+    """
+    variables = {"search": anime_name}
+    response = requests.post(url, json={"query": query, "variables": variables})
+    return response.json()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    data = None
+    if request.method == 'POST':
+        anime_name = request.form['userInput']
+        data = fetch_anime_details(anime_name)
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
