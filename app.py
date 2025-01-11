88a6b791ef75454f0d039ad4d1c31090292183ca
Priyans00 <115811175+Priyans00@users.noreply.github.com>
2025-01-11 21:40:09 +0530

    fix

--- app.py
+++ app.py
@@ -28,6 +28,38 @@ def fetch_anime_details(anime_name):
     response = requests.post(url, json={"query": query, "variables": variables})
     return response.json()
 
+def fetch_character_details(character_name):
+    url = "https://graphql.anilist.co"
+    query = """
+    query Character($search: String) {
+    Character(search:$search) {
+    age
+    dateOfBirth {
+      day
+      year
+      month
+    }
+    image {
+      large
+    }
+    description
+    gender
+    favourites
+    bloodType
+    name {
+      alternative
+      alternativeSpoiler
+      full
+    }
+    
+  }
+}
+    """
+    variables = {"search": character_name}
+    response = requests.post(url, json={"query": query, "variables": variables})
+    return response.json()
+
+
 @app.route('/',methods=['GET','POST'])
 def home():
     return render_template('home.html')
@@ -40,6 +72,14 @@ def index():
         anime_name = request.form['userInput']
         data = fetch_anime_details(anime_name)
     return render_template('index.html', data=data)
+
+@app.route('/character',methods=['GET','POST'])
+def character():
+    data = None 
+    if request.method == 'POST':
+        character_name = request.form['userInput']
+        data = fetch_character_details(character_name)
+    return render_template('character.html',data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
