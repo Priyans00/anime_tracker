2e1b1a31450ec5782f6ea492414bb8f564027834
Priyans00 <115811175+Priyans00@users.noreply.github.com>
2025-01-12 20:29:35 +0530

    fix

--- queries.py
+++ queries.py
@@ -5,23 +5,28 @@ def fetch_anime_details(anime_name):
     url = "https://graphql.anilist.co"
     query = """
     query ($search: String) {
-        Media(search: $search, type: ANIME) {
-            id
-            title {
-                romaji
-                english
-                native
-            }
-            description
-            episodes
-            genres
-            averageScore
-            coverImage {
-                large
-            }
+      Page(perPage: 50) {
+        media(search: $search, type: ANIME) {
+        id
+        title {
+          romaji
+          english
         }
-    }
-    """
+        genres
+        averageScore
+        description
+        season
+        episodes
+      
+        seasonYear
+        format
+        coverImage{
+          large
+        }
+        }
+      }
+    }"""
+
     variables = {"search": anime_name}
     response = requests.post(url, json={"query": query, "variables": variables})
     return response.json()
@@ -31,28 +36,29 @@ def fetch_character_details(character_name):
     url = "https://graphql.anilist.co"
     query = """
     query Character($search: String) {
-    Character(search:$search) {
-    age
-    dateOfBirth {
-      day
-      year
-      month
+      Page(perPage:10){
+        characters(search:$search) {
+        age
+        dateOfBirth {
+          day
+          year
+          month
+        }
+        image {
+          large
+        }
+        description
+        gender
+        favourites
+        bloodType
+        name {
+          alternative
+          alternativeSpoiler
+          full
+        }
+        } 
+      }
     }
-    image {
-      large
-    }
-    description
-    gender
-    favourites
-    bloodType
-    name {
-      alternative
-      alternativeSpoiler
-      full
-    }
-    
-  }
-}
     """
     variables = {"search": character_name}
     response = requests.post(url, json={"query": query, "variables": variables})
